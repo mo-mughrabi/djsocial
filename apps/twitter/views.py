db975892240a886b0b5785d6cd95f9423cced69d
mariusz <mariusz.kosakowski@outlook.com>
2014-02-03 01:41:55 +0100

    Half the job #2

--- apps/twitter/views.py
+++ apps/twitter/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.utils.decorators import method_decorator
+from django.views.generic import DeleteView
 from django.views.generic.base import View
 from models import Twitter, ScheduleOrder, Order
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
@@ -17,10 +20,12 @@ class DashboardView(View):
         scheduled_orders_list = ScheduleOrder.objects.filter(user=get_object_or_404(Twitter, user=request.user),
                                                         run_once=False)
         orders = Order.objects.filter(user=get_object_or_404(Twitter, user=request.user), ).order_by('-status')
+        total_pending = Order.objects.filter(user=get_object_or_404(Twitter, user=request.user),
+                                             status=Order.PENDING).count()
 
-        paginator = Paginator(scheduled_orders_list, 2)
-
+        paginator = Paginator(scheduled_orders_list, 3)
         page = request.GET.get('page')
+
         try:
             scheduled_orders = paginator.page(page)
         except PageNotAnInteger:
@@ -28,4 +33,18 @@ class DashboardView(View):
         except EmptyPage:
             scheduled_orders = paginator.page(paginator.num_pages)
 
-        return render(request, self.template_name, {'scheduled_orders': scheduled_orders, 'orders': orders})
+        return render(request, self.template_name,
+                      {'scheduled_orders': scheduled_orders, 'orders': orders, 'total_pending': total_pending})
+
+
+class DeleteScheduleOrderView(DeleteView):
+    """
+    """
+    model = ScheduleOrder
+    success_url = reverse_lazy('twitter-home')
+    template_name = 'twitter/confirm.html'
+
+    def get_object(self, queryset=None):
+        """ Hook to ensure object is owned by request.user. """
+        obj = super(DeleteScheduleOrderView, self).get_object()
+        return obj
