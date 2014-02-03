02c5d1fe0ff42074da41d99203044ffb4e911e68
mo-mughrabi <mo.mughrabi@gmail.com>
2014-02-03 15:02:42 +0300

    auto

--- apps/twitter/tasks.py
+++ apps/twitter/tasks.py
@@ -106,6 +106,7 @@ def process_scheduled_orders():
                 if last_id is None:
                     last_id = tweet.id
                 logger.info('TASK DETAILS: %s' % order.kwargs['func'])
+                #favourite
                 Order.objects.create(user=order.user, func=order.kwargs['func'], args=[tweet.id, ],
                                      schedule_order=order,
                                      kwargs={
