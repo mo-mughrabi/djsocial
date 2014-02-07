62dc3804855b9e883b4c6b641eedfcb2f8e8d7a3
mo-mughrabi <mo.mughrabi@gmail.com>
2014-02-07 13:54:23 +0300

    auto

--- apps/extras/management/commands/test_api.py
+++ apps/extras/management/commands/test_api.py
@@ -3,7 +3,8 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 import tweepy
 from tweepy import OAuthHandler, Cursor
-from apps.twitter.models import Twitter
+from apps.twitter.models import Twitter, Order
+from djorm_hstore.expressions import HstoreExpression as HE
 
 
 def get_diff(list1, list2):
@@ -15,40 +16,43 @@ class Command(BaseCommand):
     args = '<No arguments>'
 
     def handle(self, *args, **options):
-        auth = OAuthHandler(getattr(settings, 'TWITTER_CONSUMER_KEY'), getattr(settings, 'TWITTER_CONSUMER_SECRET'))
-        account = Twitter.objects.get(pk=5)
-        auth.set_access_token(account.access_token, account.secret_key)
-        api = tweepy.API(auth)
-        me = api.me()
-        print me.__dict__
-        last_id = None
-        i = 0
-        x = None
-        timeline = []
-        black_listed_words = ["RT", u"♺"]
-        try:
-            for tweet in Cursor(api.search, q='@shemalewiki', result_type='mixed', rpp=100).items(200):
-                timeline.append(tweet)
-                timeline = filter(lambda status: status.text[0] != "@", timeline)
-                timeline = filter(lambda status: not any(word in status.text.split() for word in black_listed_words),
-                                  timeline)
-                # exclude self
-                timeline = filter(lambda status: status.author.id != me.id, timeline)
+        print Order.objects.filter(func='retweet').where(
+            HE("kwargs").contains({'tweet': 'محاكمة خليجية...غازلت زوجها http://dlvr.it/4skNQD  #kuwait #الكويت'}))
 
-                if len(timeline) >= 100:
-                    break
-        except tweepy.TweepError as e:
-            print e
-
-
-
-        for tweet in timeline:
-            print tweet.id
-            print tweet.text
-            print tweet.author.screen_name
-            print tweet.author.id
-            print '++++++++++++++++++++++'
-                    # for tweet in api.search(q='shemale', result_type='mixed', since_id='428429007902998528', count='100'):
+        # auth = OAuthHandler(getattr(settings, 'TWITTER_CONSUMER_KEY'), getattr(settings, 'TWITTER_CONSUMER_SECRET'))
+        # account = Twitter.objects.get(pk=5)
+        # auth.set_access_token(account.access_token, account.secret_key)
+        # api = tweepy.API(auth)
+        # me = api.me()
+        # print me.__dict__
+        # last_id = None
+        # i = 0
+        # x = None
+        # timeline = []
+        # black_listed_words = ["RT", u"♺"]
+        # try:
+        #     for tweet in Cursor(api.search, q='@shemalewiki', result_type='mixed', rpp=100).items(200):
+        #         timeline.append(tweet)
+        #         timeline = filter(lambda status: status.text[0] != "@", timeline)
+        #         timeline = filter(lambda status: not any(word in status.text.split() for word in black_listed_words),
+        #                           timeline)
+        #         # exclude self
+        #         timeline = filter(lambda status: status.author.id != me.id, timeline)
+        #
+        #         if len(timeline) >= 100:
+        #             break
+        # except tweepy.TweepError as e:
+        #     print e
+        #
+        #
+        #
+        # for tweet in timeline:
+        #     print tweet.id
+        #     print tweet.text
+        #     print tweet.author.screen_name
+        #     print tweet.author.id
+        #     print '++++++++++++++++++++++'
+        # for tweet in api.search(q='shemale', result_type='mixed', since_id='428429007902998528', count='100'):
         #     print '--- new tweet ---'
         #     print tweet.text
         #     print tweet.id
@@ -66,4 +70,4 @@ class Command(BaseCommand):
         #     last_id = tweet.id
         #     print
 
-        print last_id
+        # print last_id
